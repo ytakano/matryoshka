0df03951e5e60229b40a4febcc46644684f31fff
Yuuki Takano <ytakanoster@gmail.com>
2018-11-22 00:09:18 +0900

    added Matryoshka.hs

---
+++ src/Matryoshka.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Matryoshka
+    ( Value(..),
+      Expr(..),
+      parse
+    ) where
+
+import           Control.Monad
+import           Control.Monad.Identity (Identity)
+import qualified Text.Parsec            as P
+
+data Value = Atom String
+           | Num Int
+           | Var String
+           | PFun String
+           | Lambda String Expr deriving (Show)
+
+data Expr = Val Value
+          | Apply Expr Expr
+          | PrimtiveFun String [Expr]
+          | If Expr Expr Expr
+          | Spawn Expr [Expr]
+          | Clone Expr [Expr]
+          | Ch Int Int
+          | Send Expr Expr
+          | Recv Expr deriving (Show)
+
+parserTop = do
+    return $ Val (Num 10)
+
+parseHelper :: P.Stream s Identity t => P.Parsec s () a -> s -> Either P.ParseError a
+parseHelper rule text = P.parse rule "(stdin)" text
+
+parse :: String -> Either P.ParseError Expr
+parse expr = parseHelper parserTop expr
